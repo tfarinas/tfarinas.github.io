05d6a309aba87bb5d98da0f16c2108fe89d9bcb1
tfarinas <tfarinas.hernandez@gmail.com>
2018-08-31 23:57:18 +0200

    updates and fix

--- resources/publications/update-references.bat
+++ resources/publications/update-references.bat
@@ -1,3 +1,3 @@
-pandoc-citeproc --bib2json articles.bib > articles.json
-pandoc-citeproc --bib2json chapters.bib > chapters.json
-pandoc-citeproc --bib2json reports.bib > reports.json
+pandoc-citeproc -j --bib2json articles.bib > articles.json
+pandoc-citeproc -j --bib2json chapters.bib > chapters.json
+pandoc-citeproc -j --bib2json reports.bib > reports.json
